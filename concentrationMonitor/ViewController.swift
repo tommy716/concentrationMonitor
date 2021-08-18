bf14ccb79150f9f74ff9a9fa2d61aedc60530606
Tommy <1999yuto@gmail.com>
2021-08-18 11:58:37 +0900

    Initial Commit

---
+++ concentrationMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  concentrationMonitor
+//
+//  Created by Tommy on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
